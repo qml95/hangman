c8fc0b42b09a5aa43dbcc2a3daa24df8bad812a5
qml95 <quentin.morino.laudrin@gmail.com>
2018-03-12 09:40:03 +0100

    init react with CRA and add Data

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { Data } from './Data';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    console.log(Data);
+    return (
+      <div className="">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,11 @@
+export const Data = [
+  'chien',
+  'chat',
+  'oiseau',
+  'poisson',
+  'requin',
+  'elephant',
+  'tigre',
+  'lion',
+  'zebre',
+];
